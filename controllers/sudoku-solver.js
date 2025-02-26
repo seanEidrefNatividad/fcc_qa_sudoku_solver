c00c0e70b470a5256b5efc93df37cc1ec58bb9ce
seanEidrefNatividad <seaneidrefnatividad@gmail.com>
2025-02-26 08:43:40 +0800

    done: checkRowPlacement function

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -7,7 +7,21 @@ class SudokuSolver {
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
+    const rowArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
+    //const column = ['1', '2', '3', '4', '5', '6', '7','8', '9'];
 
+    // let grid = {}
+    // let currPuzzle = 0
+    // row.forEach(letter => {
+    //   column.forEach(num => {
+    //     grid[letter][num] = puzzleString[currPuzzle]
+    //     currPuzzle++;
+    //   })
+    // })
+
+    const start = rowArray.indexOf(row.toUpperCase()) * 9 ;
+    const puzzleRow = puzzleString.substring(start, start+9)
+    return puzzleRow.indexOf(value) > -1 && puzzleRow[column-1] != value ? "row" : null;
   }
 
   checkColPlacement(puzzleString, row, column, value) {
