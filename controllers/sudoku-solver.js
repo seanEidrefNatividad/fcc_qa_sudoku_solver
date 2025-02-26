4a42925e41258aa798df150d675de2427758a0f6
seanEidrefNatividad <seaneidrefnatividad@gmail.com>
2025-02-26 13:50:23 +0800

    update: remove comments, fix region placement, and add initial solve code

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -8,17 +8,6 @@ class SudokuSolver {
 
   checkRowPlacement(puzzleString, row, column, value) {
     const rowArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
-    //const column = ['1', '2', '3', '4', '5', '6', '7','8', '9'];
-
-    // let grid = {}
-    // let currPuzzle = 0
-    // row.forEach(letter => {
-    //   column.forEach(num => {
-    //     grid[letter][num] = puzzleString[currPuzzle]
-    //     currPuzzle++;
-    //   })
-    // })
-
     const start = rowArray.indexOf(row.toUpperCase()) * 9 ;
     const puzzleRow = puzzleString.substring(start, start+9)
     return puzzleRow.indexOf(value) > -1 && puzzleRow[column-1] != value ? "row" : null;
@@ -26,29 +15,23 @@ class SudokuSolver {
 
   checkColPlacement(puzzleString, row, column, value) {
     const rowArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
-
     const startIndex = column % 9 == 0 ? 8 : (column % 9) - 1;
-
     let puzzleRow = '';
-
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < 9; i++) {
       if (i == 0) {
         puzzleRow += puzzleString[startIndex]
       } else {
         puzzleRow += puzzleString[startIndex + (9*i)]
       }
     }
-    //console.log(puzzleRow)
-    //console.log(puzzleRow[rowArray.indexOf(row.toUpperCase())])
-
     return puzzleRow.indexOf(value) > -1 && puzzleRow[rowArray.indexOf(row.toUpperCase())] != value ? "column" : null;
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
-    // const col = column % 9 == 0 ? 8 : (column % 9) - 1;
+    const col = column % 9 == 0 ? 8 : (column % 9) - 1;
     // let colGroupArray = [[1,2,3],[4,5,6],[7,8,9]]
     // let colGroup = colGroupArray[Math.floor(col / 3)]
-    // console.log(col)
+    //console.log(col)
 
     const rowArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
     const start = rowArray.indexOf(row.toUpperCase());
@@ -57,16 +40,75 @@ class SudokuSolver {
     const colRegion = column%3 == 0 ? 3 : column%3
     const multiplier = start%3
     const inputIndex =  ((multiplier*3) + colRegion) - 1;
-    //console.log("("+multiplier + " * " + 3+") + "+ colRegion+" = "+inputIndex)
-
     let puzzleRegion = '';
-    rowGroup.forEach(rowReg => {
+    rowGroup.forEach((rowReg,i) => {
       const startIndex = rowArray.indexOf(rowReg) * 9;
-      const puzzleRow = puzzleString.substring(startIndex, startIndex+3)
+      const colMultiplier = Math.floor(col / 3) * 3
+      const puzzleRow = puzzleString.substring(startIndex+ colMultiplier, startIndex+colMultiplier+3)
+
+      //console.log(":",rowReg,startIndex, puzzleRow, Math.floor(col / 3) * 3)
       puzzleRegion += puzzleRow
     })
+    //console.log(puzzleRegion)
+    //return puzzleRegion.indexOf(value) > -1 ? "region" : null;
 
     return puzzleRegion.indexOf(value) > -1 && puzzleRegion[inputIndex] != value ? "region" : null;
+  }
+
+  solve(puzzleString) {
+    const rowArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
+    let result = puzzleString;
+
+
+    for(let i = 0; i < puzzleString.length; i++) {
+      if (puzzleString[i] != ".") continue; 
+
+      let row = rowArray[Math.ceil((i + 1) / 9) - 1]
+      let column = (i + 1) % 9 == 0 ? 9 : (i + 1) % 9;
+      let validNum = null
+      let validNumCount = 0;
+      for(let x = 1; x < 10; x++) { 
+        //console.log(x, row, column)
+        const conflict = ['checkRowPlacement', 'checkColPlacement', 'checkRegionPlacement']
+          .map(method => this[method](result, row, column, x))
+          .filter(Boolean);
+
+        if (conflict.length == 0) {
+          //console.log(x, row, column)
+          // break;
+          //validNum = x;
+          //break;
+          //result = result.slice(0, i) + x + result.slice(i+1,i+2);
+
+          // result = result.slice(0, i) + x + result.slice(i+1);
+          //console.log(result)
+
+          validNum = x
+          validNumCount++;
+
+          //break;
+        } 
+      }
+
+      if (validNumCount == 1) {
+        result = result.slice(0, i) + validNum + result.slice(i+1);
+      } 
+
+      //console.log(validNum, row, column)
+
+      // if (validNum) {
+        // result.slice(0, i) + validNum + result.slice(i+1,i+2);
+      // } else {
+      //   result = null
+      //   return;
+      // }
+    }
+
+
+
+    console.log(result)
+    return result.indexOf('.') == -1 ? result : null
+
   }
 }
 
